0d21483e5573d0a24f6a79878c162adde53674b8
unknown <>
2017-03-28 09:13:30 +0900

    feat: add histogram, boxplot. add up to 5 year(60 month) price change as new columns docs: finish problem statement

--- main.py
+++ main.py
@@ -26,7 +26,9 @@ from sklearn.metrics import r2_score
 #database extra
 # BOJ
 
+#############################################
 ###import data 
+#############################################
 snp = quandl.get('YALE/SPCOMP') #SNP price, earnign, CPI, dividend
 # yield1 = quandl.get('ML/AAAEY') #AAA rate bond yield 1996-2017
 # conf = quandl.get('YALE/US_CONF_INDEX_VAL_INST') #institutional confidence level every 6 months 1989-
@@ -36,6 +38,11 @@ snp = quandl.get('YALE/SPCOMP') #SNP price, earnign, CPI, dividend
 # print(yield1, conf, house, wage)
 # print(snp)
 
+#////////////////////////////////////////////
+
+#############################################
+###modify datasets
+#############################################
 ###reset index
 snp = snp.reset_index(drop=True)
 
@@ -50,11 +57,13 @@ monthly_changes = (snp/snp.shift(12))-1
 monthly_changes = monthly_changes.fillna(value =0)
 monthly_changes['y'] = monthly_changes['S&P Composite'].shift(-1) #snp['S&P Composite'].pct_change(periods = 12).dropna()#.reset_index(drop = True)
 
-
 ###new col
 monthly_changes['PE Ratio (value)'] = snp['Cyclically Adjusted PE Ratio']
 monthly_changes['PER 6m change'] = (snp['Cyclically Adjusted PE Ratio']/snp['Cyclically Adjusted PE Ratio'].shift(6))-1
-
+monthly_changes['price 24m change']=(snp['S&P Composite']/snp['S&P Composite'].shift(24))-1
+monthly_changes['price 36m change']=(snp['S&P Composite']/snp['S&P Composite'].shift(36))-1
+monthly_changes['price 48m change']=(snp['S&P Composite']/snp['S&P Composite'].shift(48))-1
+monthly_changes['price 60m change']=(snp['S&P Composite']/snp['S&P Composite'].shift(60))-1
 ###drop "S&P Composite"
 delcol(monthly_changes,['CPI','Long Interest Rate'])
 
@@ -62,14 +71,34 @@ monthly_changes = monthly_changes[12:].dropna().reset_index(drop = True) #deleti
 
 ###rename columns
 
+#////////////////////////////////////////////
 
-#prrint
+#############################################
+###analysis 1: plotting 
+#############################################
+
+#print
 print(monthly_changes)
-
-
+# print snp['S&P Composite']
 ###plot
-monthly_changes.plot()
-# plt.show()
+# monthly_changes.plot()
+
+#histogram
+###have to figure out how to plot vlines on each chart
+# Q1 = np.percentile(monthly_changes,25)
+# Q3  =np.percentile(monthly_changes,75)
+# step = 1.5*(Q3 - Q1)
+# plt.axvline(Q1, color = 'b', linestyle ='dashed', linewidth =2)
+# plt.axvline(Q3, color = 'b', linestyle ='dashed', linewidth =2)
+# plt.axvline(Q1-step, color = 'r', linestyle ='dashed', linewidth =2)
+# plt.axvline(Q3+step, color = 'r', linestyle ='dashed', linewidth =2)
+monthly_changes.hist(bins = 20)
+# monthly_changes.kurtosis()
+
+#box plot
+# monthly_changes.plot.box()
+
+#plt.show()
 ###todo 
 # make a new column with discrete target values
 
@@ -93,6 +122,13 @@ print(pd.DataFrame(monthly_changes).corr())
 # estimator = grid_search.GridSearchCV(KNeighborsRegressor(), param_grid={"n_neighbors": [2,3,4,5,6,7,8,9,10]})
 estimator = linear_model.RidgeCV(alphas=[0.1, 0.5,1.0, 10.0])
 
+#////////////////////////////////////////////
+
+
+#############################################
+###analysis 2: statistics 
+#############################################
+
 ###r2 score
 def r2(key,data, regressor):
 	new_data = data.drop(key, axis = 1)
@@ -103,12 +139,13 @@ def r2(key,data, regressor):
 	score = r2_score(y_test, y_pred)
 	print("r2 score", score)
 r2('y',monthly_changes,estimator)
+# print(r2_score(monthly_changes['y'][:1040].values,estimator.predict(monthly_changes[:1040].drop('y').values)))
 
 # print(estimator.best_params_, estimator.best_estimator_)
 print(estimator.alpha_)
 
 #samples to see the result of prediction
-print estimator.predict(monthly_changes.ix[1624,:].drop('y').values) 
+# print estimator.predict(monthly_changes.ix[1624,:].drop('y').values) 
 
 
 #plot
